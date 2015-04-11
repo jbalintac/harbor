d70b73de7908be400d81330159af44ac720f4cd1
jordan <jordanbalintac08@yahoo.com>
2015-04-11 23:55:44 +0800

    heroku test

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-var express = require('express');
+// var express = require('express');
+// var app = express();
+
+// var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// var dir = process.env.OPENSHIFT_DATA_DIR || __dirname;
+
+// app.use(express.static(dir + '/app/src/'));
+// var server = app.listen(port, function(){
+
+
+// 	var host = server.address().address;
+// 	var port = server.address().port;
+// 	console.log('harbor listening at http://%s:%s', host, port);
+// });
+
+var express = require('express')
 var app = express();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var dir = process.env.OPENSHIFT_DATA_DIR || __dirname;
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/app/src'))
 
-app.use(express.static(dir + '/app/src/'));
-var server = app.listen(port, function(){
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('harbor listening at http://%s:%s', host, port);
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
